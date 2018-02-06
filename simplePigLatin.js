2900605e377d3601ba4b5ce733854e9cf734f2cc
kgorecki <krzysztof.gorecki@nokia.com>
2018-02-06 11:49:07 +0100

    improvements

--- simplePigLatin.js
+++ simplePigLatin.js
@@ -2,6 +2,7 @@ function pigIt(str){
     var space,firstWord,firstLetter;
     var position = [];
     var allWords = [];
+    var finalWord = [];
     var occurrence = str.match(/ /g);
     var addOn = 'ay';
     if (occurrence !== null) {
@@ -16,6 +17,7 @@ function pigIt(str){
         position.push(space);
         news.splice(space,1,'');
     }
+    console.log("pozycje: " + position);
     //console.log(news);
     console.log(occurrence);
     /*firstWord = news.slice(0,position[0]);
@@ -26,18 +28,42 @@ function pigIt(str){
     console.log(firstWord);
     firstWord = firstWord.join('');
     console.log(firstWord);*/
-
+    var dummy = news;
+    console.log(dummy);
     for (j=0; j<occurrence+1;j++){
-        firstWord = news.slice(0,position[j]);
+        console.log("zaczynamy " + news);
+        firstWord = dummy.slice(position[j-1],position[j]);
+        if (firstWord[0] === ''){
+            firstWord = firstWord.slice(1);
+        }
+        else {};
+        console.log("Pierwsze " + firstWord);
         firstLetter = firstWord[0];
-        firstWord = firstWord.slice(firstWord.length);
+        firstWord = firstWord.slice(1);
+        console.log(firstWord);
         firstWord = firstWord.concat(firstLetter + addOn);
+        console.log(firstWord);
         firstWord = firstWord.join('');
+        console.log(firstWord);
         console.log(firstWord.length);
         news = news.slice(firstWord.length-1);
-        console.log(news);
+        console.log("koniec " + news);
         allWords.push(firstWord);
-        console.log(allWords);
+        console.log("zpushowane " + allWords);
     }
+
+    for (var i=0; i<occurrence;i++){
+        space = news.indexOf(' ');
+        position.push(space);
+        news.splice(space,1,'');
+    }
+
+    for (var k=0; k<=occurrence;k++){
+        allWords = allWords.push()
+        space = dummy.indexOf(' ');
+        position.push(space);
+        allWords.splice(space,1,'');
+    }
+    console.log(allWords);
 }
 pigIt('Pig latin is cool')
